433fd63daa45cfc7e01f38c6d7cdbbae40e07b0c
Angeline Flora John <areas-onetime.0z@icloud.com>
2023-11-23 13:04:27 -0800

    TableViewController in MyNotes iOS app
    added table view controller to MyNotes iOS app

--- MyNotes/MyNotes/ViewControllers/NoteDetailViewController.swift
+++ MyNotes/MyNotes/ViewControllers/NoteDetailViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import CoreData
 
 class NoteDetailViewController: UIViewController {
-
+    
+    @IBOutlet var noteTitle: UITextField!
+    @IBOutlet var noteDetail: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func saveNote(_ sender: Any) {
+        //save the note into CoreData
+        //go back
+        self.navigationController?.popToRootViewController(animated: true)
+    }
 }
--- MyNotes/MyNotes/ViewControllers/NotesTableViewController.swift
+++ MyNotes/MyNotes/ViewControllers/NotesTableViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import CoreData
 
+public var allNotes = [Notes]() //notes array to store all my notes
 class NotesTableViewController: UIViewController {
 
+    @IBOutlet var notesTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         title = "My Notes"
+        notesTable.delegate = self
+        notesTable.dataSource = self
+        
+        //reload the data
+        
     }
+    
+    @IBAction func addNewNote(_ sender: Any) {
+        performSegue(withIdentifier: "ToNoteDetailVC", sender: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        notesTable.reloadData()
+    }
+}
 
+extension NotesTableViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //number of rows to be displayed in the table
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //what detail should be shown in the table cell
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //when a cell is selected highlight and segue to the note detail view controller
+        performSegue(withIdentifier: "ToNoteDetailVC", sender: nil)
+        
+        //set the note detail
+    }
 }
