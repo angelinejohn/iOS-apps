67facac6e1bc26efda0c3ca96989341b20900c0b
Angeline Flora John <areas-onetime.0z@icloud.com>
2023-11-23 12:32:50 -0800

    MyNotes iOS App initial commit
    Basic skeleton of the MyNotes iOS App

---
+++ MyNotes/MyNotes/ViewControllers/NoteDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NoteDetailViewController.swift
+//  MyNotes
+//
+//  Created by Angeline Flora John on 11/23/23.
+//
+
+import UIKit
+
+class NoteDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ MyNotes/MyNotes/ViewControllers/NotesTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NotesTableViewController.swift
+//  MyNotes
+//
+//  Created by Angeline Flora John on 11/23/23.
+//
+
+import UIKit
+
+class NotesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = "My Notes"
+    }
+
+}
