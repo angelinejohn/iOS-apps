c5c08eadff1f6470fa1ac1e77dfac21463e73cc0
Angeline Flora John <areas-onetime.0z@icloud.com>
2023-11-30 20:41:37 -0800

    MyToDos iOS app
    added priority slider to the UI

--- MyToDos/MyToDos/ViewControllers/TaskDetailViewController.swift
+++ MyToDos/MyToDos/ViewControllers/TaskDetailViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class TaskDetailViewController: UIViewController {
 
+    enum Priority: String {
+        case low, high, medium
+    }
+    
+    @IBOutlet var prioritySlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // set the prioriy picker
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveTask(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
     }
-    */
-
+    
 }
