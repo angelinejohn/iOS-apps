e02836baed70e0b5e4df4ca3e1d09093cb17ea57
Angeline Flora John <areas-onetime.0z@icloud.com>
2023-12-02 14:39:30 -0800

    Calculator iOS app
    buttons and outlets of the calculator iOS app

---
+++ Calculator/Calculator/CalculatorViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CalculatorViewController.swift
+//  Calculator
+//
+//  Created by Angeline Flora John on 12/2/23.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    // initializations and outlets
+    private var result: Double = 0
+    @IBOutlet var calculatorDisplay: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // function for the numbers
+    @IBAction func numberClicked(_ sender: UIButton) {
+        
+    }
+
+    // function for the operations
+    @IBAction func performCalculation(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func displayResult(_ sender: UIButton) {
+        
+    }
+    
+    // function to clear the last number from the display
+    @IBAction func clearLastNumber(_ sender: UIButton) {
+        
+    }
+    
+    // function to clear all the numbers from the display
+    @IBAction func clearAllNumbers(_ sender: UIButton) {
+        
+    }
+}
