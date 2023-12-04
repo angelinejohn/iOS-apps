d7371278a4eba33bee5f0d11243a3f0149134e79
Angeline Flora John <areas-onetime.0z@icloud.com>
2023-12-03 19:58:16 -0800

    Magic 8 ball iOS app
    added basic view controller and its corresponding class and connected the two

---
+++ Magic Eight Ball/Magic Eight Ball/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  Magic Eight Ball
+//
+//  Created by Angeline Flora John on 12/2/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = "Magic 8 Ball"
+    }
+    
+    @IBAction func shakeTheMagicBall(_ sender: Any) {
+    }
+}
