1acab3bb5955d341d37755c23da599c43cc7c3bc
Angeline Flora John <areas-onetime.0z@icloud.com>
2023-12-15 20:26:41 -0800

    new Location identifier iOS app
    initial commit of the new app

---
+++ Location Identifier/Location Identifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Location Identifier
+//
+//  Created by Angeline Flora John on 12/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
